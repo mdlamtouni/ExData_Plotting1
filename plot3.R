bbe2f6340865657bef8acd2b151cd73cd693afe0
mdlamtouni <mouaad.data@gmail.com>
2015-05-10 18:35:12 +0000

    plot3

--- plot3.R
+++ plot3.R
@@ -1,16 +1,16 @@
-best <- function(state, outcome) {
-        ## Read outcome data
-        outcomes <- read.csv("outcome-of-care-measures.csv")
-        ## Check that state and outcome are valid
-        if (!(state %in% outcomes[,7])) stop("invalid state")
-        ill <- switch(outcome, "heart attack"=11, "heart failure"=17, "pneumonia"=23, 0)
-        if (ill == 0) stop("invalid outcome")
-        ## Return hospital name in that state with lowest 30-day death
-        ## rate
-        range <- outcomes[outcomes[['State']]==state,]
-        nnarange <- range[range[,ill]!="Not Available",]
-        bests <- nnarange[nnarange[,ill]==min(as.numeric(as.character(nnarange[,ill]))),]
-        as.character(if(length(best)==46) bests[['Hospital.Name']]
-                     else bests[bests[,2]==min(as.character(bests[,2])),][[2]])
-}
-
+ data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",  
+                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"') 
+ data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y") 
+ data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02")) 
+ datetime <- paste(as.Date(data$Date), data$Time) 
+ data$Datetime <- as.POSIXct(datetime) 
+ with(data, { 
+     plot(Sub_metering_1~Datetime, type="l", 
+          ylab="Global Active Power (kilowatts)", xlab="") 
+     lines(Sub_metering_2~Datetime,col='Red') 
+     lines(Sub_metering_3~Datetime,col='Blue') 
+ }) 
+ legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,  
+        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) 
+ dev.copy(png, file="plot3.png", height=480, width=480) 
+ dev.off() 
